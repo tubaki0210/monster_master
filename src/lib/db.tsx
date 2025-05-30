cb28a1057c5b3fafe493ba1016f91d6c0ed26e22
Asano <asanowork0913@gmail.com>
2025-05-30 22:30:58 +0900

    remove

--- src/lib/db.tsx
+++
@@ -1,8 +0,0 @@
-import  mysql   from 'mysql2/promise'
-export const db = mysql.createPool({
-    host: "localhost",
-    // port : 3306,
-    user: "root",
-    password: "aken12aiu",
-    database: "monster_master",
-  });
