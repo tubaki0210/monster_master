3690e2146ebe822cbf281a32325495490e856ffd
Asano <asanowork0913@gmail.com>
2025-09-02 21:34:12 +0900

    Supabaseに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
-import Link from "next/link";
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className="font-serif h-screen flex justify-between container items-center m-auto font-bold">
-      <Link href='/allmonster' className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl">
+      <Link
+        href="/allmonster"
+        className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl"
+      >
         <div className="flex items-center justify-center h-full">
           <div>モンスター情報</div>
         </div>
-      </Link >
-      <Link className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl" href='/monster_combination'>
-        <div className="flex items-center justify-center h-full">
-          配合検索
-        </div>
-      </Link >
-      <Link className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl" href='/monster_master'>
+      </Link>
+      <Link
+        className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl"
+        href="/monster_combination"
+      >
+        <div className="flex items-center justify-center h-full">配合検索</div>
+      </Link>
+      {/* <Link className="bg-amber-200 w-70 h-60 rounded-3xl text-3xl shadow-2xl" href='/monster_master'>
         <div className="flex items-center justify-center h-full text-center">
           モンスターマスター
         </div>
-      </Link>
+      </Link> */}
     </div>
   );
 }
--- src/components/Sideber.tsx
+++ src/components/Sideber.tsx
@@ -1,40 +1,56 @@
-"use client"
-import React, { useState } from 'react'
-import { Close, Menu } from "@mui/icons-material"
-import Link from 'next/link'
+'use client';
+import React, { useState } from 'react';
+import { Close, Menu } from '@mui/icons-material';
+import Link from 'next/link';
 
 const Sideber = () => {
-    const [menuflag, setMenuFlag] = useState<boolean>(true);
-    const LinkList = [
-        {
-            name: "モンスター一覧",
-            path: "/allmonster"
-        },
-        {
-            name: "配合検索",
-            path: "/monster_combination"
-        },
-        {
-            name: "モンスターマスター",
-            path: "/monster_master"
-        }
-    ]
+  const [menuflag, setMenuFlag] = useState<boolean>(true);
+  const LinkList = [
+    {
+      name: 'モンスター一覧',
+      path: '/allmonster',
+    },
+    {
+      name: '配合検索',
+      path: '/monster_combination',
+    },
+    {
+      name: 'モンスターマスター',
+      path: '/monster_master',
+    },
+  ];
 
-    return (
-        <div>
-            <div className='absolute left-15 top-10 grid' onClick={() => setMenuFlag(false)}><Menu fontSize='large' /></div>
-            <div className={`w-62.5 h-screen bg-gray-200 fixed transition-all duration-300  top-0 ${menuflag ? "-left-62.5" : "left-0"}`}>
-                <div className='absolute top-3 right-2 grid' onClick={() => setMenuFlag(true)}><Close /></div>
-                <ul className='mt-15'>
-                    {LinkList.map((item, index) => (
-                        <li key={index} className='mt-7 px-7 py-4'>
-                            <Link href={item.path} className='w-full'>{item.name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <div className="z-90">
+      <div
+        className="absolute left-15 top-10 grid"
+        onClick={() => setMenuFlag(false)}
+      >
+        <Menu fontSize="large" />
+      </div>
+      <div
+        className={`w-62.5 h-screen bg-gray-200 fixed transition-all duration-300  top-0 ${
+          menuflag ? '-left-62.5' : 'left-0'
+        }`}
+      >
+        <div
+          className="absolute top-3 right-2 grid"
+          onClick={() => setMenuFlag(true)}
+        >
+          <Close />
         </div>
-    )
-}
+        <ul className="mt-15">
+          {LinkList.map((item, index) => (
+            <li key={index} className="mt-7 px-7 py-4">
+              <Link href={item.path} className="w-full">
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Sideber
+export default Sideber;
